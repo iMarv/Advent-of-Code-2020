f369e1f640af380cc0756d4cdbffa8603ef4ae4a
Marvin Altemeier <marvin.altemeier@hetzner-cloud.de>
2020-12-01 16:01:40 +0100

    Refactor day 1

--- day1.rkt
+++ day1.rkt
@@ -2,39 +2,37 @@
 
 (require threading)
 
-(define (day1-1 coins)
-    (~> coins
+(define (day1-1 expenses)
+    (~> expenses
         (get-pair 2020 _)
         multiply-list))
 
-(define (day1-2 coins)
-    (~>> coins
+(define (day1-2 expenses)
+    (~>> expenses
         get-triplet
         multiply-list))
 
-(define (get-triplet coins)
-    (let([f (first coins)])
-        (let ([pair (get-pair (- 2020 f) (list-tail coins 1))])
-          (if (> (length pair) 0)
-            (append (list f) pair)
-            (if (> (length coins) 2)
-                (get-triplet (list-tail coins 1))
+(define (get-triplet expenses)
+    (let([first-expense (first expenses)])
+        (match (get-pair (- 2020 first-expense) (list-tail expenses 1))
+            [(list a b) (list first-expense a b)]
+            [_ (if (> (length expenses) 2)
+                (get-triplet (list-tail expenses 1))
                 '()
-            ))
-        )))
+            )])
+        ))
 
-(define (get-pair target coins)
-    (if (> (length coins) 0)
-        (check-to-target target (first coins) (list-tail coins 1))
+(define (get-pair target expenses)
+    (if (> (length expenses) 0)
+        (check-to-target target (first expenses) (list-tail expenses 1))
         '()))
 
 (define (check-to-target target el trailing)
-    (let ([matches (find-match target el trailing)])
-        (if (> (length matches) 0)
-            (list el (first matches))
-            (if (> (length trailing) 0)
+    (match (find-match target el trailing)
+        [(list m) (list el m)]
+        [_ (if (> (length trailing) 0)
                 (check-to-target target (first trailing) (list-tail trailing 1))
-                '()))))
+                '())]))
 
 (define (find-match target el trailing)
     (let ([t (- target el)])
