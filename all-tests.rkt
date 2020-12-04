c2958f7b909bbd0b3c7a2e6b003e784d0e72f894
Marvin Altemeier <marvin.altemeier@hetzner-cloud.de>
2020-12-04 11:12:11 +0100

    Improve output when running all tests

--- all-tests.rkt
+++ all-tests.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(require rackunit/text-ui racket/cmdline "util-test.rkt"
+(require rackunit rackunit/text-ui racket/cmdline "util-test.rkt"
     "day1-test.rkt" "day2-test.rkt" "day3-test.rkt"
     "day4-test.rkt")
 
@@ -13,11 +13,13 @@
         ["day3" (run-tests day3-tests)]
         ["day4" (run-tests day4-tests)]
         ["all"
-            (run-tests util-tests)
-            (run-tests day1-tests)
-            (run-tests day2-tests)
-            (run-tests day3-tests)
-            (run-tests day4-tests)
+            (run-tests (test-suite "all"
+                util-tests
+                day1-tests
+                day2-tests
+                day3-tests
+                day4-tests
+            ))
         ]
         [_ (printf "No day matching '~s'~n" day)]))
 
